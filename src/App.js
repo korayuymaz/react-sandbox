58867fc4ef8fa6fc5e09301817b7015ce7bc8744
korayuymaz <korayuymaz@gmail.com>
2022-06-15 14:23:13 +0300

    Other hooks added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import UseRefExample1 from "./components/UseRefExample1";
+import UseMemoExample from "./components/UseMemoExample";
+// import UseRefExample3 from "./components/UseRefExample3";
+// import UseRefExample2 from "./components/UseRefExample2";
+// import UseRefExample1 from "./components/UseRefExample1";
 
 function App() {
   return (
     <div className="container mt-5 ml-1">
-      <UseRefExample1 />
+      <UseMemoExample />
+      {/* <UseRefExample1 /> */}
     </div>
   );
 }
