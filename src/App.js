ed110abf9ac7bf4516227047e388dd2492b78304
korayuymaz <korayuymaz@gmail.com>
2022-06-16 11:14:01 +0300

    Finished Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import UseMemoExample from "./components/UseMemoExample";
+// import UseMemoExample from "./components/UseMemoExample";
 // import UseRefExample3 from "./components/UseRefExample3";
 // import UseRefExample2 from "./components/UseRefExample2";
 // import UseRefExample1 from "./components/UseRefExample1";
+// import UseCallbackExample from "./components/UseCallbackExample";
+// import CustomHookExample1 from "./components/CustomHookExample1";
+import CustomHookExample2 from "./components/CustomHookExample2";
 
 function App() {
   return (
     <div className="container mt-5 ml-1">
-      <UseMemoExample />
-      {/* <UseRefExample1 /> */}
+      <CustomHookExample2 />
     </div>
   );
 }
